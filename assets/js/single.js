95807e41e1d8b960248dc2b52e8ebb255d85ff4c
ThuRaPhyo <thuyaphyo7877@gmail.com>
2024-06-09 15:44:41 +0630

    solved

---
+++ assets/js/single.js
@@ -0,0 +1,95 @@
+var repoNameEl = document.querySelector('#repo-name');
+var issueContainerEl = document.querySelector('#issues-container');
+var limitWarningEl = document.querySelector('#limit-warning');
+
+var getRepoName = function () {
+  // Where is this value coming from?
+  // TODO: Write your answer here
+  // This is coming from the URL search bar in the browser. It is what comes after the `?`.
+  var queryString = document.location.search;
+  var repoName = queryString.split('=')[1];
+
+  if (repoName) {
+    repoNameEl.textContent = repoName;
+
+    getRepoIssues(repoName);
+  } else {
+    // Under what condition will this run?
+    // TODO: Write your answer here
+    // When return to the homepage if there was nothing in the URL query parameter.
+    document.location.replace('./index.html');
+  }
+};
+
+var getRepoIssues = function (repo) {
+  var apiUrl = 'https://api.github.com/repos/' + repo + '/issues?direction=asc';
+
+  fetch(apiUrl).then(function (response) {
+    if (response.ok) {
+      response.json().then(function (data) {
+        displayIssues(data);
+
+        // What is this checking for? Under what condition will this be `true`?
+        // TODO: Write your answer here
+         // Since GitHub only returns 30 results at a time, we check to see if there's more than 30 by looking for a next page URL in the response headers.
+        if (response.headers.get('Link')) {
+          displayWarning(repo);
+        }
+      });
+    } else {
+      document.location.replace('./index.html');
+    }
+  });
+};
+
+var displayIssues = function (issues) {
+  // Is there a difference between this and `!issues.length`?
+  // TODO: Write your answer here
+  //check for strict equality. Using `!issues.length` works, but only because JavaScript considers `0` to be `falsy`.
+  if (issues.length === 0) {
+    issueContainerEl.textContent = 'This repo has no open issues!';
+    return;
+  }
+
+  for (var i = 0; i < issues.length; i++) {
+    var issueEl = document.createElement('a');
+    issueEl.classList = 'list-item flex-row justify-space-between align-center';
+    issueEl.setAttribute('href', issues[i].html_url);
+    issueEl.setAttribute('target', '_blank');
+
+    var titleEl = document.createElement('span');
+    titleEl.textContent = issues[i].title;
+    issueEl.appendChild(titleEl);
+
+    var typeEl = document.createElement('span');
+
+    if (issues[i].pull_request) {
+      typeEl.textContent = '(Pull request)';
+    } else {
+      typeEl.textContent = '(Issue)';
+    }
+
+    issueEl.appendChild(typeEl);
+
+    issueContainerEl.appendChild(issueEl);
+  }
+};
+
+// What does this function do?
+// TODO: Write your answer here
+ // If there's already a pull request open, it's a good idea we focus on other open issues that no one has worked on.
+var displayWarning = function (repo) {
+  limitWarningEl.textContent = 'To see more than 30 issues, visit ';
+
+  var linkEl = document.createElement('a');
+  linkEl.textContent = 'GitHub.com';
+  linkEl.setAttribute('href', 'https://github.com/' + repo + '/issues');
+  linkEl.setAttribute('target', '_blank');
+
+  // Where does this appear on the page?
+  // TODO: Write your answer here
+  // When there are more issues than what GitHub has returned, we let the user know by printing a message with a link to the page.
+  limitWarningEl.appendChild(linkEl);
+};
+
+getRepoName();
